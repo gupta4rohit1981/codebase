71d99267675222f3eebec16bb9f703ca0890ad06
gupta4rohit1981 <gupta4rohit@gmail.com>
2025-05-21 10:43:49 +0000

    test java code

---
+++ test.java
@@ -0,0 +1,11 @@
+// Simple Java program
+// FileName: "test.java"
+public class test {
+    
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+}
